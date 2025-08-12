e51e42605337f91ec7e92ff124f73f6305491911
MasegoM94 <masegomodibane@gmail.com>
2025-08-12 07:55:09 -0400

    Answering Midcourse Project Questions
    First attempt at midcourse project

--- Untitled.sql
+++ Untitled.sql
@@ -123,3 +123,115 @@ GROUP BY  year(created_at)
          
 order by 3,1,2
 ;
+
+/*
+I’d like to tell the story of our website performance improvements over the course of the first 8 months.
+Could you pull session to order conversion rates, by month?
+*/
+
+-- Month , number sessions , number orders, session to order conversion rate 
+
+SELECT YEAR(ws.created_at) as YR
+       , MONTH(ws.created_at) as MNTH 
+       , COUNT(DISTINCT ws.website_session_id) as number_of_sessions
+       , COUNT(DISTINCT o.order_id) as number_of_orders
+       , COUNT(DISTINCT o.order_id)/COUNT(DISTINCT ws.website_session_id) as session_to_order_cvr
+FROM website_sessions ws
+		LEFT JOIN 	orders o
+				ON	ws.website_session_id = o.website_session_id 
+WHERE ws.created_at < '2012-11-27'
+GROUP BY YEAR(ws.created_at) 
+       , MONTH(ws.created_at) 
+;
+
+
+/*
+For the gsearch lander test, please estimate the revenue that test earned us (Hint: Look at the increase in CVR
+from the test (Jun 19 – Jul 28), and use nonbrand sessions and revenue since then to calculate incremental value)
+*/
+
+SELECT 	yearweek(ws.created_at) as yearweek_
+		,date(min(ws.created_at)) as start_of_week
+        ,count(distinct ws.website_session_id) as number_of_sessions
+        ,count(distinct o.order_id) as number_of_orders
+        ,count(distinct o.order_id)/count(distinct ws.website_session_id) as session_to_order_cvr
+        ,sum(o.price_usd) as revenue
+FROM website_sessions ws
+	LEFT JOIN 	orders o
+			on	ws.website_session_id = o.website_session_id
+WHERE 	ws.utm_source = 'gsearch'
+	AND	ws.utm_campaign = 'nonbrand' 
+	AND	ws.created_at < '2012-07-28'
+    AND ws.created_at > '2012-06-19'
+GROUP BY yearweek(ws.created_at)
+
+;
+/*
+For the landing page test you analyzed previously, it would be great to show a full conversion funnel from each
+of the two pages to orders. You can use the same time period you analyzed last time (Jun 19 – Jul 28).
+*/
+
+-- lander page, sessions, the different pages , 
+
+WITH lander_page as (
+					SELECT wp.website_session_id
+							, min(website_pageview_id) as min_pg_id
+					FROM website_pageviews wp
+						 WHERE	wp.created_at < '2012-07-28'
+						 AND 	wp.created_at > '2012-06-19'
+					GROUP BY wp.website_session_id
+                    ),
+                    
+ distinct_lander_session as (
+							SELECT distinct lp.website_session_id
+											,wp.pageview_url
+							FROM lander_page lp
+									LEFT JOIN 	website_pageviews wp
+											ON	lp.min_pg_id = wp.website_pageview_id
+							WHERE wp.pageview_url in ('/home','/lander')
+						)
+                            
+SELECT distinct wpv.pageview_url
+       -- CASE WHEN wpv.pageview
+FROM distinct_lander_session ds
+	LEFT JOIN 	website_pageviews wpv
+			ON 	wpv.website_session_id = ds.website_session_id
+    
+
+;
+
+/*
+I’d love for you to quantify the impact of our billing test, as well. Please analyze the lift generated from the test
+(Sep 10 – Nov 10), in terms of revenue per billing page session, and then pull the number of billing page sessions
+for the past month to understand monthly impact.
+*/ 
+WITH BILLING AS 
+				(SELECT 	wp.pageview_url 
+                ,COUNT(DISTINCT wp.website_session_id) as NUMBER_OF_SESSIONS
+							,SUM(o.price_usd) as REVENUE
+                                                       ,SUM(o.price_usd) / COUNT(DISTINCT wp.website_session_id) as REVENUE_PER_PAGE_SESSION 
+				FROM website_pageviews wp
+					LEFT JOIN 	orders o
+							ON 	o.website_session_id = wp.website_session_id
+				WHERE wp.pageview_url in ('/billing-2', '/billing')
+					AND	wp.website_session_id  >= 25325
+					AND wp.created_at < '2012-11-10'
+				GROUP BY wp.pageview_url  )
+
+SELECT *
+FROM BILLING;
+
+SELECT 	wp.pageview_url 
+		,COUNT(DISTINCT wp.website_session_id) as NUMBER_OF_SESSIONS
+							,SUM(o.price_usd) as REVENUE
+                           ,SUM(o.price_usd) / COUNT(DISTINCT wp.website_session_id) as REVENUE_PER_PAGE_SESSION 
+				FROM website_pageviews wp
+					LEFT JOIN 	orders o
+							ON 	o.website_session_id = wp.website_session_id
+				WHERE wp.pageview_url in ('/billing-2', '/billing')
+					AND wp.created_at < '2012-11-27'
+                    AND wp.created_at > '2012-11-01'
+				GROUP BY wp.pageview_url 
+
+
+
